567219cf2452f0f7a8c3d82608ab70cd27ace279
Leonardo Reis <leonardo.vsreis@gmail.com>
2020-11-27 13:31:01 -0300

    exemplo de coleta de tempo

--- java/Main.java
+++ java/Main.java
@@ -7,7 +7,10 @@ public class Main {
     public static void main(String args[]) throws Exception {
         FileReader file = new FileReader(args[0]);
         javarats parser = new javarats(file, "input");
+	long beginTime = System.currentTimeMillis();
         Result r = parser.pcompilation_unit(0);
+	long endTime = System.currentTimeMillis();
+	System.out.println("Time in millis: " + (endTime - beginTime));
         if(!(r instanceof xtc.parser.ParseError))
             System.out.println("OK");
         else
--- teste funcao/Main.java
+++ teste funcao/Main.java
@@ -7,7 +7,10 @@ public class Main {
     public static void main(String args[]) throws Exception {
         FileReader file = new FileReader("input.txt");
         func parser = new func(file, "input");
-        Result r = parser.pprog(0);
+	long beginTime = System.currentTimeMillis();
+	Result r = parser.pprog(0);
+	long endTime = System.currentTimeMillis();
+	System.out.println("Time in millis: " + (endTime - beginTime));
         if(!(r instanceof xtc.parser.ParseError))
             System.out.println("OK");
         else
