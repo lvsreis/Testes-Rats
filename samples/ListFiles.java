2ea5005a9745d6360c7d9701c89e7c0b6e171a0e
Leonardo Reis <leonardo.vsreis@gmail.com>
2021-01-05 10:27:25 -0300

    ListFiles

--- samples/ListFiles.java
+++ samples/ListFiles.java
@@ -1,10 +1,10 @@
 
 import java.io.*;
 
-public class ListFile {
+public class ListFiles {
 
     public static void main(String args[]) {
-	File f = new File(".");
+	File f = new File(args[0]);
 	File inst[];
 	try {
 	    if(f.isDirectory()) {
@@ -12,38 +12,15 @@ public class ListFile {
 		inst = f.listFiles();
 		for(int i = 0; i < inst.length; i++) {
 		    pth = inst[i].getPath();
+		    System.out.println(pth);
 		}
+	    } else {
+		System.out.println(f.getPath());
 	    }
 
 	} catch(Exception e) {
 	    System.out.println("Deu merda " + e.getMessage());
 	}
-
-	
-       try{
-           if( f.isDirectory() ){
-               String pth;
-               inst = f.listFiles();
-               for(File s : inst){
-                   pth = s.getPath();
-                   System.out.print("Testando " + pth + filler(50 -pth.length()) + "[");
-                   if(adp.parseFile(s.getPath()) != null){
-                       System.out.println("  OK  ]");
-                       flips++;
-                   }else{ 
-                      System.out.println("FALHOU]");
-                      flops++;
-                   }
-               }
-               System.out.println("Total de acertos: " + flips );
-               System.out.println("Total de erros: " + flops );
-           }else{
-              System.out.println("O caminho " + f.getPath() + " não é um diretório ou não existe.");
-           }
-           
-       }catch(Exception e){
-           e.printStackTrace();
-       }
-   }
     }
 }
+
