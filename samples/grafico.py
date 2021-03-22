a351c1ce272bfc6bdc1a1038c9251e1f0cae966f
Pedro Henrique <pedroalmeidavrb@gmail.com>
2021-03-22 20:15:17 -0300

    Arrumando o gráfico

--- samples/grafico.py
+++ samples/grafico.py
@@ -7,16 +7,17 @@ dataset = open('saida.csv', 'r')
 
 for line in dataset:
     line = line.strip()  #Tira o \n no final da linha
-    X, Y, Z = line.split(';') #Quebra a linha com o marcador ;
+    X, Y = line.split(';') #Quebra a linha com o marcador ;
     x.append(X)
-    y.append(Z)
+    y.append(int(Y))
 
 dataset.close()
 
-plt.plot(x, y, c='r', marker='.')
+plt.bar(x, y)
 
 plt.title('Tempo de Execução')
 plt.xlabel('Nome arquivo')
 plt.ylabel('Tempo')
 
 plt.show()
+
