480a0df48199e266d1b1678b125016e07d939fae
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2021-04-13 10:47:22 -0300

    Pair Corrigido

--- pair/samples/Test.java
+++ pair/samples/Test.java
@@ -1,5 +1,13 @@
-import pair.Sugar;
+	import pair.Sugar;
 
-public class Test {
-    private (String, Integer) p = ("12", 34);
-}
+	public class Test {
+	    private (String, Integer) p = ("12", 34+10);
+
+	    public (int,int) divMod ( int n , int q) {
+		    return(n/q,n%q);
+	    }
+	    
+	     public static void main(String[] args) {
+	     	divMod(100,9);	    	
+	    }
+	}
