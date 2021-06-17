3464e9824342303320a6d0f104cdb838436b30e7
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2021-06-17 17:02:14 -0300

    calculadora

---
+++ compilador/Main.java
@@ -0,0 +1,19 @@
+import java.io.FileReader;
+import xtc.parser.Result;
+
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+        FileReader file = new FileReader("input.txt");
+        calculadora parser = new calculadora(file, "input");
+        long beginTime = System.currentTimeMillis();
+        Result r = parser.pcompile(0);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time in millis: " + (endTime - beginTime));
+        if(!(r instanceof xtc.parser.ParseError))
+            System.out.println("OK");
+        else
+            System.out.println("NOT OK");
+    }
+}
